9e2eef1ffe6162ca98042159ae719eec94912ef9
apase <aypase@gmail.com>
2025-07-20 16:58:41 +0100

    deploy to GCP

--- 0_provider.tf
+++ 0_provider.tf
@@ -11,4 +11,4 @@ provider "google-beta" {
   credentials = file("gcp-key.json")   # This should match the service account key file in the pipeline
   project     = var.project_id         # Use a variable for flexibility
   region      = var.region             # Use a variable for region
-}
+}                                      #fixed github secret
