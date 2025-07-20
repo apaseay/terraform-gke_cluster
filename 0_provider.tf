4c85a627ef590bc00c671f8755215b05fd3ee3a2
apase <aypase@gmail.com>
2025-07-20 17:48:21 +0100

    fixed provider.tf

--- 0_provider.tf
+++ 0_provider.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.44.0"
+    }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
       version = "6.44.0"
     }
   }
 }
 
+provider "google" {
+  credentials = file("gcp-key.json")
+  project     = var.project_id
+  region      = var.region
+}
+
 provider "google-beta" {
-  credentials = file("gcp-key.json")   # This should match the service account key file in the pipeline
-  project     = var.project_id         # Use a variable for flexibility
-  region      = var.region             # Use a variable for region
-}                                      #fixed github secret
+  credentials = file("gcp-key.json")
+  project     = var.project_id
+  region      = var.region
+}
